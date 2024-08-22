219d88f63997cae39d01c8eb277bb1850a7590b2
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-08-23 01:10:20 +0700

    first commit

--- utils/clodinary.go
+++ utils/clodinary.go
@@ -4,19 +4,41 @@ import (
 	"context"
 	"fmt"
 	"mime/multipart"
+	"os"
 
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/joho/godotenv"
 )
 
 var cld *cloudinary.Cloudinary
 
-func InitCloudinary(cloudName, apiKey, apiSecret string) error {
-	var err error
+// InitCloudinary initializes the Cloudinary client using environment variables.
+func InitCloudinary() error {
+	// Muat variabel lingkungan dari file .env
+	err := godotenv.Load()
+	if err != nil {
+		return fmt.Errorf("error loading .env file: %v", err)
+	}
+
+	// Ambil kredensial dari variabel lingkungan
+	cloudName := os.Getenv("CLOUDINARY_CLOUD_NAME")
+	apiKey := os.Getenv("CLOUDINARY_API_KEY")
+	apiSecret := os.Getenv("CLOUDINARY_API_SECRET")
+
+	if cloudName == "" || apiKey == "" || apiSecret == "" {
+		return fmt.Errorf("one or more environment variables are not set")
+	}
+
 	cld, err = cloudinary.NewFromParams(cloudName, apiKey, apiSecret)
-	return err
+	if err != nil {
+		return fmt.Errorf("error creating Cloudinary client: %v", err)
+	}
+
+	return nil
 }
 
+// UploadImageToCloudinary uploads an image to Cloudinary and returns the URL.
 func UploadImageToCloudinary(file multipart.File) (string, error) {
 	uploadResult, err := cld.Upload.Upload(context.Background(), file, uploader.UploadParams{
 		Folder: "your-folder", // Optional: specify a folder in Cloudinary
