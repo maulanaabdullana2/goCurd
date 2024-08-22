1b9598a1506901b2464c03a9ed11c81c30977c6b
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-08-23 02:18:54 +0700

    tes

--- utils/oauth2.go
+++ utils/oauth2.go
@@ -4,13 +4,20 @@ import (
 	"context"
 	"os"
 
+	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
 var GoogleOauthConfig *oauth2.Config
 
-func InitOAuth2() {
+func InitOAuth2() error {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		return err
+	}
+
+	// Initialize Google OAuth2 configuration
 	GoogleOauthConfig = &oauth2.Config{
 		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
@@ -18,6 +25,7 @@ func InitOAuth2() {
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 		Endpoint:     google.Endpoint,
 	}
+	return nil
 }
 
 func GetGoogleUserInfo(ctx context.Context, code string) (*oauth2.Token, error) {
